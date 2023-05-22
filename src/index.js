4485f9e6b8f3f527f5403ae997d6e37269b8a50f
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-22 09:59:53 -0300

    Trocando BrownserRouter por HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import FilmsProvider from './context/FIlmsProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <FilmsProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </FilmsProvider>,
 );
